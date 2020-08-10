378ab766d322bbcec9310be9ad7e401f54e54089
D.Yang <koyangslash@gmail.com>
2020-08-10 12:24:41 +0800

    init

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ function getLastestVersion($page, repo, type) {
   let version;
   try {
     const url = typeVersionUrl[type];
-    console.log(url);
-    if (url === null || url === undefined) {
-      return;
-    }
-    url.replace(/:repo/g, repo);
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    if (request.status === 200) {
-      const json = xmlHttp.responseText;
-      console.log(request.responseText);
-      version = JSON.parse(json).value.replace("v", "");
+    if (url !== null && url !== undefined) {
+      url.replace(/:repo/g, repo);
+      xmlHttp.open("GET", url, false);
+      xmlHttp.send(null);
+      if (request.status === 200) {
+        const json = xmlHttp.responseText;
+        version = JSON.parse(json).value.replace("v", "");
+      }
     }
   } catch (e) {
     console.error("get lastest version of artifact error", e);
